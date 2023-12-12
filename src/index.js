4aac764a72f3ed887c02c2000320aeff1d76381f
benardop <benard.oc.p@gmail.com>
2023-12-12 09:27:04 -0300

    Update tweet components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Tweet() {
+  return (
+    <div className='tweet'>
+      <Avatar />
+      <div className='content'>
+        <Author/>
+        <Message/>
+        <Time/>
+      </div>
+    </div>
+  )
+}
+
+function Avatar() {
+  return (
+    <img
+      src='https://www.gravatar.com/avatar/nothing'
+      alt='avatar'
+      className='avatar' />
+  )
+}
+
+function Message() {
+  return (
+    <div className='message'>
+      This is less than 140 characters
+    </div>
+  )
+}
+
+function Author() {
+  return (
+    <span>
+       <span className='name'>Benard</span>
+       <span className='handle'>@bepacho</span>
+    </span>
+  )
+}
+
+const Time = () => (
+  <span className='time'>3h ago</span>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Tweet />
+)
